5d4b08033a34569cd0a6fd6481558462528e038a
MachDomo <dominic.c.ma@gmail.com>
2017-08-23 15:42:49 -0700

    "added extend function"

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,17 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  /// Do something
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
-
+// use 'This' keyword
+var extend = function(to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
