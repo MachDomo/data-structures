6792ca094c2d6a104fae025842f5f537e5bfc292
MachDomo <dominic.c.ma@gmail.com>
2017-08-23 19:24:51 -0700

    "added enqueue function to queueMethods"

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,7 +1,7 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = {'storage': {}};
+  var someInstance = {'storage': {}, 'count': 0};
   extend(someInstance, queueMethods);
   return someInstance;
 };
@@ -10,8 +10,9 @@ var queueMethods = {
   size: function() {
     return Object.keys(this.storage).length;
   },
-  enqueue: function() {
-
+  enqueue: function(value) {
+    this.storage[this.size() + this.count] = value;
+    this.count++;
   },
   dequeue: function() {
 
