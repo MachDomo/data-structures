9e8b69b8dd6d4b90566bcb23b3e5ed949a11e7ef
MachDomo <dominic.c.ma@gmail.com>
2017-08-24 16:25:46 -0700

    "add addtotail and remove head"

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -2,14 +2,49 @@ var LinkedList = function() {
   var list = {};
   list.head = null;
   list.tail = null;
+  var arr = [];
 
   list.addToTail = function(value) {
+    arr.push(value);
+    if (list.head === null) {
+      list.head = Node(value);
+      list.tail = list.head;
+    } else if(list.head === list.tail) {
+      list.head.next = Node(value);
+      list.tail = list.head.next;
+    } else {
+      list.tail.next = Node(value);
+      list.tail = list.tail.next;
+    }
   };
 
+  /*
+    list = {list.head: {value: "first", next: {value: "second", next: {value: "third", next: null}}}, list.tail: {value: "third", next: null}
+    */
+
   list.removeHead = function() {
+    // Find the current head
+    // Remove current head
+    // Set next as current head
+    var value = list.head.value;
+    list.head = list.head.next;
+
+    return value;
+
   };
 
   list.contains = function(target) {
+    // should return a boolean
+    // recursively check from head.values to head.next values
+    var currentObject = list.head;
+    while(currentObject !== list.tail) {
+      if(currentObject.value === target) {
+        return true;
+      } else {
+        currentObject = currentObject.next;
+      }
+    }
+    return false;
   };
 
   return list;
@@ -27,3 +62,6 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+/* Linkedlist = {head: {value: value, next: null}, tail: {value: value, next: null}} */
+
