0026ee5263c9b1a883cf0736fcaa48a46b50d445
MachDomo <dominic.c.ma@gmail.com>
2017-08-23 20:29:46 -0700

    "used _.extend instead of self created extend function"

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -2,7 +2,7 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   var someInstance = {'storage': {}, 'count': 0};
-  extend(someInstance, queueMethods);
+  _.extend(someInstance, queueMethods);
   return someInstance;
 };
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,3 @@
-var extend = function(to, from) {
-  for (var key in from) {
-    to[key] = from[key];
-  }
-};
 
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
@@ -11,7 +6,7 @@ var Stack = function() {
 
 
   /// Do something
-  extend(someInstance, stackMethods);
+  _.extend(someInstance, stackMethods);
 
 
   return someInstance;
