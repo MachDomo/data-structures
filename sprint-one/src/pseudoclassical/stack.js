bee9ed41b93d303bf20567e0bc2969ec6f3bead3
MachDomo <dominic.c.ma@gmail.com>
2017-08-24 09:53:51 -0700

    "Added framework for pseudoclassical style and size function"

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,19 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // this = Object.create(this.prototype)
+  this.storage = {};
+  // return this;
 };
 
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
 
+Stack.prototype.push = function() {
+
+};
+
+Stack.prototype.pop = function() {
+
+};
