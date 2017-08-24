1b39199a0bcc9cca0cc30273aface50c0a158d92
MachDomo <dominic.c.ma@gmail.com>
2017-08-23 19:18:03 -0700

    "added object to return in stack as well as extend to queue methods"

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,21 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {'storage': {}};
+  extend(someInstance, queueMethods);
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  size: function() {
+
+  },
+  enqueue: function() {
+
+  },
+  dequeue: function() {
+
+  }
+};
 
 
