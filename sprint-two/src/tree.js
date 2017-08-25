2303af9d289e63cfcfdf5f156db8ae49247f05d7
MachDomo <dominic.c.ma@gmail.com>
2017-08-24 19:46:50 -0700

    "_.extend tree methods to tree"

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -4,7 +4,7 @@ var Tree = function(value) {
 
   // your code here
   newTree.children = null;  // fix me
-
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -21,3 +21,13 @@ treeMethods.contains = function(target) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+/*
+Making a family tree.
+
+A tree class, in functional with shared methods style, with the following properties:
+.children property, an array containing a number of subtrees
+.addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
+A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
+What is the time complexity of the above functions?
+*/
