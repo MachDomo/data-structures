547d435cabc56a27b52a0090979f5f8451bafa7c
MachDomo <dominic.c.ma@gmail.com>
2017-08-25 22:02:14 -0700

    "add removeNode and add Edge"

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -9,7 +9,7 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this[node] = node;
+  this[node] = {value: node};
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
@@ -19,33 +19,48 @@ Graph.prototype.contains = function(target) {
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  delete this.node;
-
+  if (typeof this[node].edge === 'number') {
+    var otherEdge = this[node].edge;
+    delete this[otherEdge].edge;
+  }
+  delete this[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  if (this[fromNode].edge === toNode && this[toNode].edge === fromNode) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  // add edge property to fromNode, set reference to toNode
+  this[fromNode].edge = this[toNode].value;
+  this[toNode].edge = this[fromNode].value;
+  // add edge property to toNode, set reference to fromNode
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this[fromNode].edge;
+  delete this[toNode].edge;
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
 
-var graph = new Graph(); // {}
-graph.addNode(1); // {1:1}
-console.log(graph.contains(1)); //true
+// var graph = new Graph(); // {}
+// graph.addNode(1); // {1:1}
+// console.log(graph.contains(1)); //true
 
 
 // Representing how a collection of websites (or the entire world wide web) link to each other, and many other things
