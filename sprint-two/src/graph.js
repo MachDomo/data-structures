f82b71d4f3a82dbeaa6ea8b661d8e077c6d8f241
MachDomo <dominic.c.ma@gmail.com>
2017-08-25 21:26:13 -0700

    "add addNode and contains"

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,19 +2,25 @@
 
 // Instantiate a new graph
 var Graph = function() {
-  
+  // this = Object.create(Graph.prototype);
+
+  // return this;
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this[node] = node;
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
-Graph.prototype.contains = function(node) {
+Graph.prototype.contains = function(target) {
+  return this.hasOwnProperty(target);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  delete this.node;
+
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -37,4 +43,20 @@ Graph.prototype.forEachNode = function(cb) {
  * Complexity: What is the time complexity of the above functions?
  */
 
+var graph = new Graph(); // {}
+graph.addNode(1); // {1:1}
+console.log(graph.contains(1)); //true
 
+
+// Representing how a collection of websites (or the entire world wide web) link to each other, and many other things
+
+// Implement a graph class, in pseudoclassical style, with the following properties:
+// It is an undirected graph. It does not have to be 'connected'.
+// An .addNode() method that takes a new node and adds it to the graph
+// A .contains() method that takes any node and returns a boolean reflecting whether it can be found in the graph
+// A .removeNode() method that takes any node and removes it from the graph, if present. All edges connected to that Node are removed as well.
+// An .addEdge() method that creates a edge (connection) between two nodes if they both are present within the graph
+// A .hasEdge() method that returns a boolean reflecting whether or not two nodes are connected
+// A .removeEdge() method that removes the connection between two nodes
+// A .forEachNode() method that traverses through the graph, calling a passed in function once on each node
+// What is the time complexity of the above functions?
