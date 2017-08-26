4778e0c1a4b17db6b4781a827ffe9bb4539ca29f
MachDomo <dominic.c.ma@gmail.com>
2017-08-25 20:40:40 -0700

    "add contains function and passed all tests"

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,9 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code heret
-  newTree.children = [];  // fix me
+  // your code here
+  newTree.children = [];// fix me
+  newTree.parent = null;
   _.extend(newTree, treeMethods);
   return newTree;
 };
@@ -15,27 +16,33 @@ treeMethods.addChild = function(value) {
   // assign value key to object based on value parameter
   // put object in tree.children
   // document.body.childNodes
-  var node = {};
-  node.value = value;
-  node.children = [];
-  this.children.push(node);
+  // var node = {};
+  // node.value = value;
+  // node.children = [];
+  // _.extend(node, treeMethods);
+
+  // this.children.push(node);
+  // return node;
+  var child = Tree(value);
+  child.parent = this;
+  return this.children.push(child);
+
 };
+
 
 treeMethods.contains = function(target) {
+  // take an input (target)
+  var isTrue = false;
+  var x = function(node) {
+    if (node.value === target) {
+      isTrue = true;
+    } else {
+      for (let i = 0; i < node.children.length; i++) {
+        x(node.children[i]);
+      }
+    }
+  };
+  x(this);
+  return isTrue;
 };
 
-// Master only
-
-/*
- * Complexity: What is the time complexity of the above functions?
- */
-
-/*
-Making a family tree.
-
-A tree class, in functional with shared methods style, with the following properties:
-.children property, an array containing a number of subtrees
-.addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
-A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
-What is the time complexity of the above functions?
-*/
