07b0f74552dd1faf26f29256a05084e0765e9d90
MachDomo <dominic.c.ma@gmail.com>
2017-08-25 23:40:43 -0700

    "add for set contains and remove"

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,31 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // Check if in set.storage
+  // if no, push to set.storage
+  if (!(this.contains())) {
+    this.storage.push(item);
+  }
 };
 
 setPrototype.contains = function(item) {
+  // find item in the set
+  return _.contains(this.storage, item);
+  // if yes, return true, else return false
 };
 
 setPrototype.remove = function(item) {
+
+  var index = _.indexOf(this.storage, item);
+  delete this.storage[index];
+  // should delete that item from set
+
 };
 
 /*
